46e9f91383fd1273d971d9a46ca876c3da82d5f1
@amb97 <annika.bell@gmail.com>
2019-05-13 15:51:31 +1200

    second attempt at changing temp units to metric + deletion of previously commented code

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -8,7 +8,7 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";
 
 exports.getWeather = function(req, res) {
 	var city = req.query.zip;
@@ -28,7 +28,7 @@ exports.getWeather = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
@@ -39,37 +39,5 @@ exports.getWeather = function(req, res) {
 
 };
 router.get('/getWeather', exports.getWeather);
-
-
-/*exports.getWeather2 = function(req, res) {
-	var city = req.query.zip;
-	if( (city === null) || (typeof(city) === 'undefined') ) {
-		return res.status(400).send('city missing');
-	}
-
-	var aurl = OPENWEATHERURL + '&q=' + city + ',nz';
-
-	request({
-		method: 'GET',
-        url: aurl,
-  		json: true
-    }, function(err, resp, body) {
-    	if(err) {
-    		res.status(400).send('Failed to get the data');
-    		//console.error("Failed to send request to openweathermap.org", err);
-    	} else {
-    		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
-    			var response = {city: body.name, weather: weath};
-    			return res.status(200).send(response);
-    		} else {
-                return res.status(400).send({msg:'Failed'});
-            }
-    	}
-    });
-
-};
-router.get('/getWeather2', exports.getWeather2);*/
-
 
 exports.router = router;
