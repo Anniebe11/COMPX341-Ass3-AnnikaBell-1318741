ef664cc1f8827ab4778cbf3e684285890a0afa8c
@amb97 <annika.bell@gmail.com>
2019-05-13 16:02:47 +1200

    undoing metrics edits

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,4 +1,3 @@
-// randome comment so file is changed
 
 var express = require('express');
 var router = express.Router();
@@ -8,7 +7,7 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
 
 exports.getWeather = function(req, res) {
 	var city = req.query.zip;
@@ -28,7 +27,7 @@ exports.getWeather = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
